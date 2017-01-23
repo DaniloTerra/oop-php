512c37cbe2d49fe1218c945893905acbff19265a
DaniloTerra <danilo_terra@live.com>
2017-01-23 18:51:18 -0200

    Adicionado classe Car e SportCar

--- cap7/index.php
+++ cap7/index.php
@@ -1,3 +1,22 @@
 <?php
 
-phpinfo();
+$ferrari = new SportCar();
+$ferrari->setModel('Ferrari Enzo');
+echo $ferrari->sayHello();
+
+class Car
+{
+	private $model;
+
+	public function setModel($model)
+	{
+		$this->model = $model;
+	}
+
+	public function sayHello()
+	{
+		return "Hello, I'm a " . $this->model . " car!";
+	}
+}
+
+class SportCar extends Car {}
