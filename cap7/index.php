2f48ea7da3ab81f38d9382756d532c32a94fc8ea
DaniloTerra <danilo_terra@live.com>
2017-01-23 19:10:14 -0200

    Override de métodos

--- cap7/index.php
+++ cap7/index.php
@@ -2,21 +2,168 @@
 
 $ferrari = new SportCar();
 $ferrari->setModel('Ferrari Enzo');
+$ferrari->setStyle('Really Fast');
 echo $ferrari->sayHello();
+echo "<br>";
+echo $ferrari->run();
 
 class Car
 {
-	private $model;
+	protected $model;
 
-	public function setModel($model)
+	public final function setModel($model)
 	{
 		$this->model = $model;
 	}
 
-	public function sayHello()
+	public final function sayHello()
 	{
-		return "Hello, I'm a " . $this->model . " car!";
+		return "Hello! I'm a " . $this->model . " car.";
+	}
+
+	public function run()
+	{
+		return "I'm running without breaking the law";
 	}
 }
 
-class SportCar extends Car {}
+class SportCar extends Car
+{
+	private $style;
+
+	public final function setStyle($style)
+	{
+		$this->style = $style;
+	}
+
+	public function run()
+	{
+		return "Hello! I'm " . $this->model . ". Drive me " . $this->style . ". Break the law!";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $ferrari = new SportCar();
+// $ferrari->setModel('Ferrari Enzo');
+// $ferrari->setStyle('Fast and Furious');
+// echo $ferrari->sayHello();
+// echo "<br>";
+// echo $ferrari->driveItWithStyle();
+
+
+// class Car
+// {
+// 	protected $model;
+
+// 	public function setModel($model)
+// 	{
+// 		$this->model = $model;
+// 	}
+
+// 	public function sayHello()
+// 	{
+// 		return "Hello! I'm a " . $this->model . " car.";
+// 	}
+// }
+
+// class SportCar extends Car
+// {
+// 	private $style;
+
+// 	public function setStyle($style)
+// 	{
+// 		$this->style = $style;
+// 	}
+
+// 	public function driveItWithStyle()
+// 	{
+// 		return "I'm a " . $this->model . ". So drive me " . $this->style;
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $ferrari = new SportCar();
+// $ferrari->setModel('Ferrari Enzo');
+// echo $ferrari->sayHello();
+
+// class Car
+// {
+// 	private $model;
+
+// 	public function setModel($model)
+// 	{
+// 		$this->model = $model;
+// 	}
+
+// 	public function sayHello()
+// 	{
+// 		return "Hello, I'm a " . $this->model . " car!";
+// 	}
+// }
+
+// class SportCar extends Car {}
